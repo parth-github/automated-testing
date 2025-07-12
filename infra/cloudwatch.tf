9ab4e6c1632a1e66d8cfdbd5e0fe56550e234ea9
Smriti <linuxriti@gmail.com>
2025-07-13 03:34:41 +0530

    checkov sec kms

--- infra/cloudwatch.tf
+++ infra/cloudwatch.tf
@@ -1,5 +1,28 @@
 resource "aws_kms_key" "cloudwatch_logs" {
-  description = "KMS key for CloudWatch Logs encryption"
+  description         = "KMS key for CloudWatch Logs encryption"
+  enable_key_rotation = true
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Id": "cloudwatch-logs-kms-policy",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "*"
+      },
+      "Action": [
+        "kms:Encrypt",
+        "kms:Decrypt",
+        "kms:GenerateDataKey*",
+        "kms:DescribeKey"
+      ],
+      "Resource": "*"
+    }
+  ]
+}
+EOF
 }
 
 resource "aws_cloudwatch_log_group" "ecs-log-group" {
