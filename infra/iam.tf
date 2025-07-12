49d3bc53f474e47e40b27a5497b4bac1a12ab011
Smriti <linuxriti@gmail.com>
2025-07-12 21:38:48 +0530

    reformat

--- infra/iam.tf
+++ infra/iam.tf
@@ -1,6 +1,5 @@
 resource "aws_iam_role" "ecs_task_execution_role" {
   name = "ecsTaskExecutionRole"
-
   assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [{
@@ -11,10 +10,9 @@ resource "aws_iam_role" "ecs_task_execution_role" {
       Effect = "Allow",
     }]
   })
-
-  }
+}
   
-  resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
-    role       = aws_iam_role.ecs_task_execution_role.name
-    policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
--- infra/variable.tf
+++ infra/variable.tf
@@ -10,3 +10,7 @@ variable "vpc_id" {}
 variable "public_subnet_ids" {
   type = list(string)
 }
+
+variable "public_subnet_ids" {
+  type = list(string)
+}
