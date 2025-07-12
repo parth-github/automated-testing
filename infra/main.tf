245944d9536fb17dcd66e4c1d37becfdeca80c99
Smriti <linuxriti@gmail.com>
2025-07-12 20:33:43 +0530

    tf modules

--- infra/main.tf
+++ infra/main.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = var.aws_region
 }
 
-module "vpc" {
-  source = "./vpc"
-}
+# module "vpc" {
+#   source = "./modules"
+# }
 
-module "ecs" {
-  source = "./ecs"
-  vpc_id             = module.vpc.vpc_id
-  public_subnet_ids  = module.vpc.public_subnet_ids
-  cluster_name       = var.ecs_cluster_name
-}
+# module "ecs" {
+#   source = "./ecs"
+#   vpc_id             = module.vpc.vpc_id
+#   public_subnet_ids  = module.vpc.public_subnet_ids
+#   cluster_name       = var.ecs_cluster_name
+# }
